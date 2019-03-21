cfa563ee61477e31798139b2589dbb1c6a83dc7b
robert kim <jrheonghwan@naver.com>
2019-03-21 17:54:47 +0900

    change extra area for member value

--- yolo.py
+++ yolo.py
@@ -46,6 +46,7 @@ class YOLO(object):
         self.anchors = self._get_anchors()
         self.sess = K.get_session()
         self.boxes, self.scores, self.classes = self.generate()
+        self.extra_area = 20
 
     def _get_class(self):
         classes_path = os.path.expanduser(self.classes_path)
@@ -145,10 +146,10 @@ class YOLO(object):
             label_size = draw.textsize(label, font)
 
             top, left, bottom, right = box
-            top = max(0, np.floor(top - 30).astype('int32'))
-            left = max(0, np.floor(left - 30).astype('int32'))
-            bottom = min(image.size[1], np.floor(bottom + 30).astype('int32'))
-            right = min(image.size[0], np.floor(right + 30).astype('int32'))
+            top = max(0, np.floor(top - self.extra_area).astype('int32'))
+            left = max(0, np.floor(left - self.extra_area).astype('int32'))
+            bottom = min(image.size[1], np.floor(bottom + self.extra_area).astype('int32'))
+            right = min(image.size[0], np.floor(right + self.extra_area).astype('int32'))
             print(label, (left, top), (right, bottom))
 
             if top - label_size[1] >= 0:
